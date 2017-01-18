c72dd6740e9ef044caf439a97572bad2514bddf4
Tony Bowden <tony@tmtm.com>
2017-01-18 14:35:59 +0000

    Turn on archiving

--- scraper.rb
+++ scraper.rb
@@ -2,12 +2,13 @@
 # encoding: utf-8
 # frozen_string_literal: true
 
+require 'pry'
 require 'scraped'
 require 'scraperwiki'
-require 'pry'
 
-require 'open-uri/cached'
-OpenURI::Cache.cache_path = '.cache'
+# require 'open-uri/cached'
+# OpenURI::Cache.cache_path = '.cache'
+require 'scraped_page_archive/open-uri'
 
 def scrape(h)
   url, klass = h.to_a.first
