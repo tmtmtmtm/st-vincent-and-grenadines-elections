d24f679e5e0d137fb37583ffd5df8ba2fa4db3bd
Tony Bowden <tony@tmtm.com>
2017-01-18 15:42:39 +0000

    Don't archive by default
    We won't be running this scraper regularly, and we already have an
    archive of it, so turn off archiving by default.

--- scraper.rb
+++ scraper.rb
@@ -6,9 +6,9 @@ require 'pry'
 require 'scraped'
 require 'scraperwiki'
 
-# require 'open-uri/cached'
-# OpenURI::Cache.cache_path = '.cache'
-require 'scraped_page_archive/open-uri'
+require 'open-uri/cached'
+OpenURI::Cache.cache_path = '.cache'
+# require 'scraped_page_archive/open-uri'
 
 def scrape(h)
   url, klass = h.to_a.first
